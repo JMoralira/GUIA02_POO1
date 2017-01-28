1dadda423b4de89575c26d95c5ca584591cd76f4
Jose Lira <Jose Lira@Jose-PC>
2017-01-28 09:43:26 -0600

    Esta es la Guia 02 de POO1

---
+++ src/guia02_poo1/GUIA02_POO1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia02_poo1;
+
+/**
+ *
+ * @author Jose Lira
+ */
+public class GUIA02_POO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
